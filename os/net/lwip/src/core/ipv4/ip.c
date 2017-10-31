df087b360e65de616b5faacb8c58c258a7a3a6d9
Krzysztof Antoszek <k.antoszek@samsung.com>
2017-10-31 15:17:12 +0100

    Route multicast connections
    Signed-off-by: Krzysztof Antoszek <k.antoszek@samsung.com>

--- os/net/lwip/src/core/ipv4/ip.c
+++ os/net/lwip/src/core/ipv4/ip.c
@@ -164,7 +164,8 @@ struct netif *ip_route(ip_addr_t *dest)
 	for (netif = netif_list; netif != NULL; netif = netif->next) {
 		/* network mask matches? */
 		if (netif_is_up(netif)) {
-			if (dest->addr == 0xffffffff || ip_addr_netcmp(dest, &(netif->ip_addr), &(netif->netmask))) {
+			if (dest->addr == 0xffffffff || ip_addr_ismulticast(dest) ||
+          ip_addr_netcmp(dest, &(netif->ip_addr), &(netif->netmask))) {
 				/* return netif on which to forward IP packet */
 				return netif;
 			}
